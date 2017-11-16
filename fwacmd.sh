9f076dd5b22355c24653d8c1fbc23bd84e8b9f28
Eddie Lien <eddie.lien@gmail.com>
2017-11-16 13:29:02 +0800

    missing --yes
    keep script running without asking Y/N

--- fwacmd.sh
+++ fwacmd.sh
@@ -40,7 +40,7 @@ function install() {
     sudo docker run hello-world # Should run!
 
     # Install RVM
-    sudo apt-get install libgdbm-dev libncurses5-dev automake libtool bison libffi-dev
+    sudo apt-get install libgdbm-dev libncurses5-dev automake libtool bison libffi-dev --yes
     gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
     curl -sSL https://get.rvm.io | bash -s stable
     source ~/.rvm/scripts/rvm
